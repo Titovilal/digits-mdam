6cf04bd4fd1230c1713d5c2b4bdb68c57634bed7
Titovilal <scastellsjuan@gmail.com>
2024-04-23 23:19:01 +0200

    add some resources and set project arch

---
+++ utils/models.py
@@ -0,0 +1,24 @@
+# # Importar las bibliotecas necesarias
+# from sklearn import neighbors, neural_network, linear_model, svm, cluster
+# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+# # K-Nearest Neighbors (KNN)
+# knn = neighbors.KNeighborsClassifier()
+
+# # Multi-Layer Perceptron (MLP)
+# mlp = neural_network.MLPClassifier()
+
+# # Linear Regression
+# linear = linear_model.LinearRegression()
+
+# # Quadratic Discriminant Analysis
+# quadratic = QuadraticDiscriminantAnalysis()
+
+# # Support Vector Machine (SVM)
+# svc = svm.SVC()
+
+# # K-Means
+# kmeans = cluster.KMeans(n_clusters=3)  # puedes cambiar el número de clusters
+
+# # Entrenar el modelo KNN
+# knn.fit(X_train, y_train)
