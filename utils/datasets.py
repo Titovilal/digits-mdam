eeb241b37dcace28247d5537717a22d5f5735627
Titovilal <scastellsjuan@gmail.com>
2024-04-28 04:23:22 +0200

    Add mlp graphic and many other things

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,4 +1,6 @@
 import scipy.io.matlab as matlab
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 def load_dataset():
@@ -9,3 +11,24 @@ def load_dataset():
     labels = mat["labs"] - 1
     new_labels = [0 if label == 0 else 1 for label in labels]
     return data, new_labels
+
+
+def show_digit(index, data, labels):
+    """Shows a digit given the index in the dataset and its label"""
+    digit = data[index]
+    label = labels[index]
+    # Reshape the digit data to its original 28x28 shape
+    digit_image = np.reshape(digit, (28, 28)).T
+    # Display the digit
+    plt.figure(figsize=(3, 3))
+    plt.imshow(digit_image, cmap=plt.cm.gray_r)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(f'Label: {label} ({"only zeros" if label == 0 else "other numbers"})')
+    plt.show()
+
+
+def get_incorrect_predictions(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    hits = predictions == y_test
+    return np.where(hits == False)[0]
--- utils/visualization.py
+++ utils/visualization.py
@@ -1,24 +1,38 @@
 import matplotlib.pyplot as plt
-import seaborn as sns  # Importa la librería Seaborn para obtener una paleta de colores
+import seaborn as sns
+
+
 def plot_MLP(mlp_accuracys):
-    plt.plot(range(1, len(mlp_accuracys)+1), mlp_accuracys, label='MLP Accuracy')
+    plt.plot(range(1, len(mlp_accuracys) + 1), mlp_accuracys, label="MLP Accuracy")
     plt.ylabel("Accuracy")
     plt.xlabel("Repetition number")
     plt.show()
 
-def plot_KNN_scores_combined(knn_cross_values_list):
+
+def plot_f1(f1_scores, xlabel="", title=""):
     plt.figure(figsize=(10, 6))
-    colors = sns.color_palette("husl", len(knn_cross_values_list))
-
-    for i, knn_cross_values in enumerate(knn_cross_values_list):
-        plt.plot(range(1, len(knn_cross_values) + 1), knn_cross_values, label=f'Repetition {i + 1}', color=colors[i])
-
+    colors = sns.color_palette("pastel", len(f1_scores))
+    # Grafica los valores de F1 para cada valor de k
+    for i, f1_score in enumerate(f1_scores):
+        plt.plot(
+            range(1, len(f1_score) + 1),
+            f1_score,
+            label=f"Repetition {i + 1}",
+            color=colors[i],
+        )
     # Calcula la media de los valores de F1 para cada valor de k
-    mean_f1_scores = [sum(scores) / len(scores) for scores in zip(*knn_cross_values_list)]
-    plt.plot(range(1, len(mean_f1_scores) + 1), mean_f1_scores, label='Media', linestyle='--', color='black')
-
+    mean_f1_scores = [sum(scores) / len(scores) for scores in zip(*f1_scores)]
+    # Grafica la media de los valores de F1
+    plt.plot(
+        range(1, len(mean_f1_scores) + 1),
+        mean_f1_scores,
+        label="Media",
+        linestyle="--",
+        color="black",
+    )
+    # Muestra la gráfica
     plt.legend(loc="lower right")
-    plt.xlabel("K")
-    plt.ylabel("F1 score")
-    plt.title("Combined KNN F1 Scores")
+    plt.xlabel(xlabel)
+    plt.ylabel("F1")
+    plt.title(title)
     plt.show()
